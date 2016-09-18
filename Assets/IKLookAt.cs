340dd2efce4b3711034cf7bd4813847a07ca8b69
MiyaraDaichi <miyara1114@docomo.ne.jp>
2016-09-19 05:38:50 +0900

    [Add] Walk
    Unityちゃんが歩くように設定。

---
+++ Assets/IKLookAt.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKLookAt : MonoBehaviour
+{
+    private Animator avator;
+    public Transform lookAtObj = null;
+
+    [SerializeField, Range(0, 1)]
+    private float lookAtWeight = 1.0f;
+    [SerializeField, Range(0, 1)]
+    private float bodyWeight = 0.4f;
+    [SerializeField, Range(0, 1)]
+    private float headWeight = 0.7f;
+    [SerializeField, Range(0, 1)]
+    private float eyesWeight = 0.5f;
+    [SerializeField, Range(0, 1)]
+    private float clampWeight = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        avator = GetComponent<Animator>();
+        if (lookAtObj == null)
+        {
+            lookAtObj = Camera.main.transform;
+        }
+    }
+
+    void OnAnimatorIK(int layorIndex)
+    {
+        if (avator)
+        {
+            avator.SetLookAtWeight(lookAtWeight, bodyWeight, headWeight, eyesWeight, clampWeight);
+            avator.SetLookAtPosition(lookAtObj.position);
+        }
+    }
+}
